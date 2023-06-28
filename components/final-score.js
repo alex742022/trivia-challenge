497ea1fa91ba93741dfe948d4ae5f96c05297499
alex742022 <alexis.aquino.lumbera7@gmail.com>
2023-06-28 19:46:54 +0800

    declare map method to fetch all the data

--- components/final-score.js
+++ components/final-score.js
@@ -1,23 +1,49 @@
-
-//this property "finalScore came from final-results page"
-import Image from "next/image"
-import Logo2 from "../public/images/logo/logo_zeniark2.png"
-export default function FinalScore({finalScore, lengthOfArray}) {
+import Image from "next/image";
+// use for decode the intity
+import he from "he"
+import Logo2 from "../public/images/logo/logo_zeniark2.png";
+//these all property are came from show-results component"
+export default function FinalScore({
+  finalScore,
+  lengthOfArray,
+  allDataResults,
+  answeredOfUser,
+}) {
+  console.log(allDataResults);
+  console.log(answeredOfUser);
   return (
     <div className="max-w-[600px] relative mx-auto bg-white px-[15px] py-[20px] sm:py-[30px] sm:px-[50px] mt-[100px]">
-    <Image
-      src={Logo2}
-      width={100}
-      height={100}
-      alt="Logo"
-      className="w-[50px] sm:w-[70px] absolute"
-    />
-    <h1 className="text-[30px] text-center">Final Results</h1>
-    <hr className="h-[2px] my-[50px]" />
-    <div className="text-center">
-      <h1 className="text-[42px] font-semibold">{finalScore} / <span className="text-[38px]">{lengthOfArray}</span> </h1>
-      <p className="text-[20px]">Your Score</p>
+      <Image
+        src={Logo2}
+        width={100}
+        height={100}
+        alt="Logo"
+        className="w-[50px] sm:w-[70px] absolute"
+      />
+      <h1 className="text-[30px] text-center">Final Results</h1>
+      <hr className="h-[2px] my-[50px]" />
+      <div className="text-center">
+        <h1 className="text-[42px] font-semibold">
+          {finalScore} / <span className="text-[38px]">{lengthOfArray}</span>
+        </h1>
+        <p className="text-[20px]">Your Score</p>
+      </div>
+
+      <div>
+        {allDataResults.map(({correct_answer, question}, index) => {
+          return (
+            <div className="my-[10px]">
+              <hr className="h-[2px]" />
+              <p>{index + 1}. {he.decode(question)}</p>
+              <div className="flex gap-[10px]">
+              <small className="italic">The correct answer is <span className={correct_answer === "True" ? `text-[green]` : `text-[red]`}>{correct_answer}.</span></small>
+              <small className="italic">Your answer is <span className={answeredOfUser[index] === "True" ? `text-[green]` : `text-[red]`}>{answeredOfUser[index]}.</span></small>
+              </div>
+            
+            </div>
+          );
+        })}
+      </div>
     </div>
-  </div>
-  )
+  );
 }
