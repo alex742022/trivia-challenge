bc36170b68778f3146b8406ca111ecc93042c00c
alex742022 <alexis.aquino.lumbera7@gmail.com>
2023-06-28 13:11:08 +0800

    add imort image

---
+++ components/final-score.js
@@ -0,0 +1,22 @@
+
+//this property "finalScore came from final-results page"
+import Image from "next/image"
+export default function FinalScore({finalScore}) {
+  return (
+    <div className="w-[900px] relative mx-auto bg-white py-[30px] px-[50px]">
+    <Image
+      src="/../public/images/logo/logo_zeniark2.png"
+      width={100}
+      height={100}
+      alt="Logo"
+      className="w-[80px] absolute"
+    />
+    <h1 className="text-[40px] text-center">Final Results</h1>
+    <hr className="h-[2px] my-[50px]" />
+    <div className="text-center">
+      <h1 className="text-[40px]">{finalScore}</h1>
+      <p className="text-[20px]">Your Score</p>
+    </div>
+  </div>
+  )
+}
