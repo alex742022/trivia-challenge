75d009f5d5458d969ec01d7a583ac481327eaef8
alex742022 <alexis.aquino.lumbera7@gmail.com>
2023-06-28 19:50:03 +0800

    passed propert into another component and added some style

--- components/show-results.js
+++ components/show-results.js
@@ -1,7 +1,13 @@
 // imported the final-results file
-import FinalResults from "../pages/final-results";
+import FinalScore from "../components/final-score";
 import { useState } from "react";
-export default function FinalResultsContainer({ showResults, lengthOfArray }) {
+// import to pass the data into final-results
+export default function FinalResultsContainer({
+  showResults,
+  lengthOfArray,
+  allDataResults,
+  answeredOfUser
+}) {
   const [showNotif, setShowNotif] = useState(true);
   const showTheResult = () => {
     setShowNotif(false);
@@ -9,12 +15,15 @@ export default function FinalResultsContainer({ showResults, lengthOfArray }) {
   return showNotif ? (
     <div className="max-w-[400px] bg-white mx-auto mt-[100px] py-[20px] flex flex-col items-center justify-center gap-[20px]">
       <h1 className="text-[20px]">Your Done!</h1>
-      <button onClick={showTheResult} className="text-[20px] brandColorBackground text-white py-[10px] px-[30px] rounded-lg">
+      <button
+        onClick={showTheResult}
+        className="text-[20px] brandColorBackground text-white py-[10px] px-[30px] rounded-lg"
+      >
         Show the result
       </button>
     </div>
   ) : (
-    //past the property into finala result
-    <FinalResults finalScore={showResults} lengthOfArray={lengthOfArray} />
+    //past the property into final score component
+    <FinalScore finalScore={showResults} lengthOfArray={lengthOfArray} allDataResults={allDataResults} answeredOfUser={answeredOfUser}/>
   );
 }
