d3d2883271efffd3933e468357a34ae1eb08fb21
alex742022 <alexis.aquino.lumbera7@gmail.com>
2023-06-28 18:59:50 +0800

    remove useless file

--- pages/final-results/index.js
+++
@@ -1,8 +0,0 @@
-// imported the final-score component to pass the property and also to display
-import FinalScore from "../../components/final-score"
-// this "finalScore" came from show-results component
-export default function FinalResults({finalScore, lengthOfArray }) {
-  return (
-   <FinalScore finalScore={finalScore} lengthOfArray={lengthOfArray}/>
-  )
-}
