20787758d15b2f8e245183349b81ba90c2420073
alex742022 <alexis.aquino.lumbera7@gmail.com>
2023-06-28 11:17:56 +0800

    added comments

--- pages/questions/index.js
+++ pages/questions/index.js
@@ -1,12 +1,15 @@
+// imported the component to pass the data came from json file
 import Questions from "../../components/questions-container";
+// imported the json file
 import DataQuestions from "../api/test-questions.json"
 export default function QuestionsPage({DataQuestions}) {
+  //destructur
   const { results } = DataQuestions;
   return <Questions results={results}/>;
 }
 
 //SSR
-
+//fetch the json data
 export async function getServerSideProps() {
   if (!DataQuestions) {
     return {
