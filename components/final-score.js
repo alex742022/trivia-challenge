bf3c6bf272b497c037239806fe99edd496f0f2e9
alex742022 <alexis.aquino.lumbera7@gmail.com>
2023-06-28 17:23:18 +0800

    add some styles

--- components/final-score.js
+++ components/final-score.js
@@ -2,20 +2,20 @@
 //this property "finalScore came from final-results page"
 import Image from "next/image"
 import Logo2 from "../public/images/logo/logo_zeniark2.png"
-export default function FinalScore({finalScore}) {
+export default function FinalScore({finalScore, lengthOfArray}) {
   return (
-    <div className="w-[900px] relative mx-auto bg-white py-[30px] px-[50px]">
+    <div className="max-w-[600px] relative mx-auto bg-white px-[15px] py-[20px] sm:py-[30px] sm:px-[50px] mt-[100px]">
     <Image
       src={Logo2}
       width={100}
       height={100}
       alt="Logo"
-      className="w-[80px] absolute"
+      className="w-[50px] sm:w-[70px] absolute"
     />
-    <h1 className="text-[40px] text-center">Final Results</h1>
+    <h1 className="text-[30px] text-center">Final Results</h1>
     <hr className="h-[2px] my-[50px]" />
     <div className="text-center">
-      <h1 className="text-[40px]">{finalScore}</h1>
+      <h1 className="text-[42px] font-semibold">{finalScore} / <span className="text-[38px]">{lengthOfArray}</span> </h1>
       <p className="text-[20px]">Your Score</p>
     </div>
   </div>
