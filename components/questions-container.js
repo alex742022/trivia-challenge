9e6218ab1cb15df2537689801cf20e197b29e4c9
alex742022 <alexis.aquino.lumbera7@gmail.com>
2023-06-28 08:13:00 +0800

    changed the path of src

--- components/questions-container.js
+++ components/questions-container.js
@@ -1,7 +1,31 @@
+import Image from "next/image";
+// import CheckIcon from "../public/images/icons"
 export default function QuestionsContainer() {
   return (
-    <div className="w-[900px] h-[700px] bg-white">
-
+    <div className="w-[900px] h-[700px] bg-white px-[50px] py-[30px]">
+      <div className="flex justify-between items-center">
+        <div className="flex items-center gap-[20px]">
+          <Image
+            src="/../public/images/logo/logo_zeniark2.png"
+            width={60}
+            height={60}
+            alt="Logo"
+          />
+          <h2>Category:</h2>
+        </div>
+        <div>1 of 10</div>
+      </div>
+      <hr className="h-[2px] mt-[20px] mb-[20px]" />
+      <div className="m-auto w-[70%] h-[400px] bg-slate-200 flex items-center">
+        <h1 className="text-[50px] text-center">
+          Green is almost as big as Africa.
+        </h1>
+      </div>
+      <hr className="h-[2px] mt-[20px]" />
+      <div>
+        <button className="bg-[green] py-[10px]">True</button>
+        <button className="bg-[red]">False</button>
+      </div>
     </div>
-  )
+  );
 }
